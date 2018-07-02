c6e2e2fe283cd245c3a53672eed53f22cdc4bbf1
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-07-01 22:28:46 -0700

    salt_empty test

--- tests/testthat/test-delete.R
+++ tests/testthat/test-delete.R
@@ -8,3 +8,12 @@ test_that("salt_na returns vector of same size", {
     expect_true(any(is.na(x)), info = str_glue("battery test: {n}"))
   })
 })
+
+test_that("salt_empty returns vector of same size", {
+  salt_empty_res <- map(battery, salt_empty)
+  imap(salt_empty_res, function(x, n) expect_is(x, class = "character", info = n))
+  walk2(salt_empty_res, battery_lengths, expect_length)
+  imap(salt_empty_res, function(x, n) {
+    expect_true(any(!nzchar(x)), info = str_glue("battery test: {n}"))
+  })
+})
