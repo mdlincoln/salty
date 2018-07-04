9fdd4a85708d1bc7dbf180e592b2ee0bcd117265
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-07-04 12:18:07 -0700

    setup salt_insert to work with helper function

--- R/salt_insert.R
+++ R/salt_insert.R
@@ -4,13 +4,14 @@
 #'
 #' @param x A vector. This will always be coerced to character during salting.
 #' @param p A number between 0 and 1. Percent of values in `x` that should be salted.
+#' @param n A postiive integer. Number of times to add new values from `insertions` into selected values in `x`
 #' @param insertions A [shaker] function, or a character vector if you want to
 #'   manually supply your own list of characters.
 #'
 #' @return A character vector the same length as `x`
 #'
 #' @export
-salt_insert <- function(x, p = 0.2, insertions) {
+salt_insert <- function(x, p = 0.2, n = 1, insertions) {
   stopifnot(length(x) > 0)
   xm <- as.character(x)
 
@@ -24,23 +25,21 @@ salt_insert <- function(x, p = 0.2, insertions) {
   xi <- setdiff(p_indices(x, p), m_nulls)
 
   # For each selected element of x, in what string position should the new character be inserted?
-  x_si <- purrr::map_int(nchar(x[xi]), sample.int, size = 1)
+  x_si <- purrr::map(nchar(xm[xi]), sample.int, size = n)
 
   # Generate new values with inserted characters
-  replacements <- purrr::map2_chr(x[xi], x_si, function(xc, si) {
-    x_head <- stringr::str_sub(xc, end = si)
-    x_tail <- stringr::str_sub(xc, start = si + 1)
-    stringr::str_c(x_head, insertions(l = 1), x_tail)
+  replacements <- purrr::map2_chr(xm[xi], x_si, function(xc, si) {
+    multi_incorporate(xc, insertions, si)
   })
 
-  x[xi] <- replacements
-  x
+  xm[xi] <- replacements
+  xm
 }
 
-salt_insert_puncutation <- function(x, p = 0.2) {
-  salt_insert(x, p, insertions = shaker$punctuation)
+salt_insert_puncutation <- function(x, p = 0.2, n = 1) {
+  salt_insert(x, p, n, insertions = shaker$punctuation)
 }
 
-salt_insert_letters <- function(x, p = 0.2) {
-  salt_insert(x, p, insertions = letters)
+salt_insert_letters <- function(x, p = 0.2, n = 1) {
+  salt_insert(x, p, n, insertions = letters)
 }
--- R/utils.R
+++ R/utils.R
@@ -24,3 +24,42 @@ mirror <- function(v) {
   names(res) <- c(all_values, all_names)
   unique(res)
 }
+
+# Given strings x and y and a pair of insertion positions, add y into x. Y will
+# be inserted in between pos1 and pos2
+single_incorporate <- function(x, y, pos1, pos2) {
+  assertthat::assert_that(assertthat::is.string(x))
+  assertthat::assert_that(assertthat::is.string(y))
+  assertthat::assert_that(assertthat::is.count(pos1))
+  assertthat::assert_that(assertthat::is.count(pos2))
+  assertthat::assert_that(pos2 > pos1)
+
+  head_segment <- stringr::str_sub(x, start = 1L, end = pos1)
+  tail_segment <- stringr::str_sub(x, start = pos2, end = -1L)
+  stringr::str_c(head_segment, y, tail_segment)
+}
+
+multi_incorporate <- function(x, insertions, positions) {
+  pos <- sort(positions)
+  assertthat::assert_that(all(positions %in% seq_len(nchar(x))))
+
+  # After each loop, increase the adjustment size by the number of characters
+  # inserted. For each subsequent index from the character indices of the
+  # original string, increment position by one to account for those added
+  # characters.
+  adjustment_size <- 0
+  for (i in seq_along(pos)) {
+    # Select new characters to insert
+    new_chars <- insertions(1)
+
+    # Adjust insertion index based on prior adjustment size
+    adjusted_si <- pos[i] + adjustment_size
+
+    # Add new substring
+    x <- single_incorporate(x, new_chars, adjusted_si, adjusted_si + 1)
+
+    # Increase adjustment size for next round
+    adjustment_size <- nchar(new_chars)
+  }
+  x
+}
