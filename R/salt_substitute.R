49b57a61c3e312a6eb828c2b4d22d461a24f75db
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-07-04 23:37:02 -0700

    fill out salt substitute

--- R/salt_substitute.R
+++ R/salt_substitute.R
@@ -8,5 +8,28 @@
 #'
 #' @export
 salt_substitute <- function(x, substitutions, p = 0.2, n = 1) {
+  assertthat::assert_that(length(x) > 0)
+  xm <- as.character(x)
 
+  # If a character vector is provided for insertions, then turn it into a shaker function
+  if (is.character(substitutions))
+    substitutions <- fill_shakers(substitutions)
+
+  # Find any empty or NA values.
+  m_nulls <- which(is.na(xm) | !nzchar(xm))
+  # Skip m_nulls when picking which values of x to modify
+  xi <- setdiff(p_indices(xm, p), m_nulls)
+
+  selected_x <- xm[xi]
+
+  # For each selected element of x, in what string positions should new characters be inserted?
+  x_si <- pic_char_indices(selected_x, n)
+
+  # Generate new values with inserted characters
+  replacements <- purrr::map2_chr(selected_x, x_si, function(xc, si) {
+    multi_incorporate(xc, substitutions, si, occlude = TRUE)
+  })
+
+  xm[xi] <- replacements
+  xm
 }
