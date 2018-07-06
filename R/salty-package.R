f5fd8685f1ac8229cf277d607916bcfd720215b5
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-07-06 11:52:55 -0700

    misnamed functions
    Used str_ when I should have used salt_

--- R/salty-package.R
+++ R/salty-package.R
@@ -2,20 +2,20 @@
 #'
 #' Insert, delete, replace, and substitute bits of your data with messy values.
 #'
-#' - [str_insert] will insert new characters into some of the values of `x`. All
+#' - [salt_insert] will insert new characters into some of the values of `x`. All
 #' the original characters of the original values will be maintained.
 #'
-#' - [str_substitute] will substitute some characters in some of the values of
+#' - [salt_substitute] will substitute some characters in some of the values of
 #' `x` in place of some of the original characters.
 #'
-#' - [str_replace] will replace some characters in some of the values of `x`.
-#' Unlike [str_substitute], [str_replace] does conditional replacement dependent
+#' - [salt_replace] will replace some characters in some of the values of `x`.
+#' Unlike [salt_substitute], [salt_replace] does conditional replacement dependent
 #' on the original values of `x`, such as changing capitalization or simulating
 #' OCR errors based on certain character combinations.
 #'
-#' - [str_delete] will remove some characters in the values of `x`
+#' - [salt_delete] will remove some characters in the values of `x`
 #'
-#' - [str_na] and [str_empty] will replace some values of `x` with `NA` or with
+#' - [salt_na] and [salt_empty] will replace some values of `x` with `NA` or with
 #' empty strings.
 #'
 #' @docType package
