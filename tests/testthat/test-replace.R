4cb889771916f457f354fe95b568c84ef333cdcf
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-07-06 12:20:19 -0700

    baseic replacement tests

---
+++ tests/testthat/test-replace.R
@@ -0,0 +1,17 @@
+replace_test <- function(selected_shaker, shaker_name) {
+  context(str_glue("salt_replace: {shaker_name}"))
+
+  test_that(str_glue("replace {shaker_name}"), {
+    replace_res <- map(battery, function(x) {
+      salt_replace(x, replacements = selected_shaker, p = 0.5, rep_p = 1)
+    })
+    imap(replace_res, function(x, n) expect_is(x, class = "character", info = n))
+    walk2(replace_res, battery_lengths, expect_length)
+  })
+
+  test_that("error on zero-length input", {
+    expect_error(salt_replace(zero_length, replacements = selected_shaker))
+  })
+}
+
+imap(replacement_shaker, replace_test)
