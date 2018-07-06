5a8aeb7d949a6965ee19be7a46ec1caa2d01edd4
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-07-06 11:34:42 -0700

    invert ocr errors

--- data-raw/read_ocr_errors.R
+++ data-raw/read_ocr_errors.R
@@ -1,3 +1,5 @@
 # Read ocr errors into a dataframe
-replacement_ocr_errors <- tibble::deframe(read.csv("data-raw/ocr_errors.csv", stringsAsFactors = FALSE))
+replacement_ocr_errors_raw <- read.csv("data-raw/ocr_errors.csv", stringsAsFactors = FALSE)
+named_reps <- tibble::deframe(replacement_ocr_errors_raw)
+replacement_ocr_errors <- purrr::set_names(x = names(named_reps), nm = unname(named_reps))
 devtools::use_data(replacement_ocr_errors, overwrite = TRUE, internal = TRUE)
