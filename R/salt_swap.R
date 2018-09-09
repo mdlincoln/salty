35510a144046314b97faef3aedeb54095798f4ed
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-09-09 06:57:42 -0400

    rename paramater for swaps
    This brings it in to line with the style for the other salt verbs

--- R/salt_swap.R
+++ R/salt_swap.R
@@ -1,6 +1,7 @@
 #' Randomly swap out entire values in a vector
 #'
 #' @inheritParams salt_insert
+#' @param swaps Values to be swapped out
 #'
 #' @return A character vector the same length as `x`
 #'
@@ -10,9 +11,9 @@
 #'        "Nunc finibus tortor a elit eleifend interdum.",
 #'        "Maecenas aliquam augue sit amet ultricies placerat.")
 #'
-#' swaps <- c("foo", "bar", "baz")
+#' new_values <- c("foo", "bar", "baz")
 #'
-#' salt_swap(x, swaps, p = 0.5)
+#' salt_swap(x, swaps = new_values, p = 0.5)
 salt_swap <- function(x, insertions, p) {
   assertthat::assert_that(length(x) > 0)
   xm <- as.character(x)
