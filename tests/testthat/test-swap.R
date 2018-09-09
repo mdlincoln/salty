76b85c28d85789d61d93598588b132dc8bd7ad7a
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-09-09 07:48:00 -0400

    test salt_swap

---
+++ tests/testthat/test-swap.R
@@ -0,0 +1,24 @@
+context("swap")
+
+swap_test <- function(selected_shaker, shaker_name) {
+  context(str_glue("salt_swap: {shaker_name}"))
+
+  test_that(str_glue("swap {shaker_name}"), {
+    swap_res <- map(battery, function(x) {
+      salt_swap(x, swaps = selected_shaker, p = 0.5)
+    })
+    imap(swap_res, function(x, n) expect_is(x, class = "character", info = n))
+    walk2(swap_res, battery_lengths, expect_length)
+    imap(swap_res, function(x, n) {
+      expect_true(any(map_lgl(x, function(y) {
+        any(inspect_shaker(selected_shaker) %in% y)
+      })), info = str_glue("shaker name: {shaker_name} - battery test: {n}"))
+    })
+  })
+
+  test_that("error on zero-length input", {
+    expect_error(salt_swap(zero_length, swaps = selected_shaker))
+  })
+}
+
+imap(shaker, swap_test)
