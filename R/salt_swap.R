3dd6b33f6ca510c52f174ffbedd756e738ea36e1
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-09-09 07:48:14 -0400

    fix up var names so swap tests pass

--- R/salt_swap.R
+++ R/salt_swap.R
@@ -1,5 +1,10 @@
 #' Randomly swap out entire values in a vector
 #'
+#' Because `swaps` can be provided by either a character vector or a function
+#' that returns a character vector, `salt_swap` can be fruitfully used in
+#' conjunction with the [charlatan::charlatan] package to intersperse real data with
+#' simulated data.
+#'
 #' @inheritParams salt_insert
 #' @param swaps Values to be swapped out
 #'
@@ -14,15 +19,17 @@
 #' new_values <- c("foo", "bar", "baz")
 #'
 #' salt_swap(x, swaps = new_values, p = 0.5)
-salt_swap <- function(x, insertions, p) {
+salt_swap <- function(x, swaps, p = 0.2) {
   assertthat::assert_that(length(x) > 0)
   xm <- as.character(x)
 
   # If a character vector is provided for insertions, then turn it into a shaker function
-  if (is.character(insertions))
-    insertions <- fill_shakers(insertions)
+  if (is.character(swaps))
+    swaps <- fill_shakers(swaps)
 
   xi <- p_indices(xm, p)
 
-  xm[xi] <- insertions(xi)
+  xm[xi] <- swaps(l = length(xi))
+
+  xm
 }
